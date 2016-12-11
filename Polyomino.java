fb73bf4892b1ae928171df2bcbd10a40f9b48a8e
fandejuni <t.dardinier@gmail.com>
2016-12-11 14:58:25 +0100

    Polyomino test

--- Polyomino.java
+++ Polyomino.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 public class Polyomino {
-    
-    public static void main(String[] args) {
-        System.out.println("Test2");
-    }
+	
+    public Set<List<Integer>> squares;
+	
+	public Polyomino(String s) {
+        squares = new HashSet<List<Integer>>();
+        int i = 1;
+        int a = 0;
+        int b = 0;
+        boolean premier = true;
+        while (s.charAt(i) != ']') {
+            char c = s.charAt(i);
+            //if (c == ')') {
+               // squares.add(
+            i++;
+        }
+        System.out.println("?");
+        System.out.println(s);
+	}
+
 }
