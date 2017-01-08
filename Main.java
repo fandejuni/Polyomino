35eb754bc7acf46c49d3710be307ddfa611f8097
fandejuni <t.dardinier@gmail.com>
2017-01-08 02:26:52 +0100

    Corrected important bugs; HI and VI working

--- Main.java
+++ Main.java
@@ -10,10 +10,10 @@ class Main {
         for (Polyomino p : l) {
             p.reflection_horizontal();
         }
-//        Polyomino.draw(l);
+        //Polyomino.draw(l);
         List<List<Polyomino>> tout = new LinkedList<List<Polyomino>>();
         //tout.add(Manipulate.generateAllFreePolyominoes(4,3));
-        tout.add(Manipulate.allFixedRedeimer(5));
+        tout.add(Manipulate.allFixedRedeimer(12));
         //tout.add(Manipulate.HI(7));
         //tout.add(Manipulate.VI(7));
         for (List<Polyomino> all : tout) {
@@ -22,7 +22,7 @@ class Main {
             }
             System.out.println(all);
             System.out.println(all.size());
-            Manipulate.draw(all);
+            //Manipulate.draw(all);
         }
 
         Set<Integer> X = new HashSet<Integer>();
@@ -31,7 +31,7 @@ class Main {
             X.add(new Integer(i));
         }
         
-        System.out.println(ExactCover.generate(1,5,2));
+        //System.out.println(ExactCover.generate(1,5,2));
 
         C.add(new LinkedList<Integer>(Arrays.asList(3, 5, 6)));
         C.add(new LinkedList<Integer>(Arrays.asList(1, 4, 7)));
