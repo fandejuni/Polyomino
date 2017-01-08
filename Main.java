b0db01266adfb28aedbb1dc1c5fbc4a80812dcc0
Thomas Cambier <Thomas Cambier>
2017-01-08 23:09:47 +0100

    Debug Task 6

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ class Main {
         //Polyomino.draw(l);
         List<List<Polyomino>> tout = new LinkedList<List<Polyomino>>();
 
-        tout.add(Manipulate.generateAllFixedPolyominoes(4, 3));
+/*        tout.add(Manipulate.generateAllFixedPolyominoes(4, 3));
         tout.add(Manipulate.generateAllFreePolyominoes(3,3));
         tout.add(Manipulate.generic(7, "Fixed"));
         tout.add(Manipulate.HX_VX(12));
@@ -22,7 +22,7 @@ class Main {
         tout.add(Manipulate.generic(12, "A"));
         tout.add(Manipulate.generic(12, "D"));
         tout.add(Manipulate.generic(13, "R"));
-        tout.add(Manipulate.generic(25, "R2"));
+        tout.add(Manipulate.generic(25, "R2"));*/
 
         for (List<Polyomino> all : tout) {
             for (Polyomino p: all) {
@@ -47,25 +47,19 @@ class Main {
 		};
 
         //System.out.println(ExactCover.resolve(conf));
-		int[][] conf2 = new int[][]{
-			{ 0, 0, 1, 0, 1, 1, 0},
-			{ 1, 0, 0, 1, 0, 0, 0},
-			{ 0, 1, 1, 0, 0, 1, 0},
-			{ 1, 0, 0, 1, 0, 0, 1},
-			{ 0, 1, 0, 0, 0, 0, 1},
-			{ 0, 0, 0, 1, 1, 0, 1},
-		};
 
-        //System.out.println(ExactCover.resolve(conf2));
 		
         /*System.out.println(ExactCover.generateSubsets(1,7,2));
         int [][] M = ExactCover.generateMatrix(ExactCover.generateSubsets(1,7,2));
         ExactCover.printMatrix(M);
         System.out.println(ExactCover.resolve(M));*/
         
-		System.out.println(ExactCover.generateAllSubsets(6));
+		/*System.out.println(ExactCover.generateAllSubsets(6));
 		int [][] M = ExactCover.generateMatrix(ExactCover.generateAllSubsets(6));
 		ExactCover.printMatrix(M);
-        System.out.println(ExactCover.resolve(M));
+        System.out.println(ExactCover.resolve(M));*/
+		
+		ColumnObject H = DancingLinks.toDancingLinks(conf);
+		System.out.println(DancingLinks.exactCover(H));
     }
 }
